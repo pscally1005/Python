9e35b2222858eb24acbe733b811698ddf9399289
pscally1005 <mets1005@yahoo.com>
2022-05-16 08:22:39 -0400

    made board scalable

--- Tick_Tack_Toe.py
+++ Tick_Tack_Toe.py
@@ -1,13 +1,36 @@
+from array import array
 from os import system
 import random
+import getch
+
+# user inputs size of the board
+def input_size() :
+    system('clear')
+    print('\nWelcome to Tic-Tac-Toe!')
+    print('\nPlease Enter a Board Size from 3 to 9: ', end = ' ')
+    size = getch.getche()
+    while (size.isnumeric() == False) or (int(size) < 3) or (int(size) > 9) :
+        system('clear')
+        print('\nWelcome to Tic-Tac-Toe!')
+        print('\nError: Board size invalid', end = '')
+        print('\nPlease Enter a Board Size from 3 to 9: ', end = ' ')
+        size = getch.getche()
+    return size
 
 # prints the board every time a move is made
 def board(b) :
     system('clear')
-    print('\nWelcome to Tick-Tac-Toe!')
-    print('You will be player "X".  The computer will be "O"\n')
+    print('\nWelcome to Tic-Tac-Toe!')
+    print('\nYou will be player X.  The computer will be O')
+    print('You win by connecting ' + str(len(b)) + ' X\'s in a row. ', end = ' ')
+    print('You lose if the computer connects ' + str(len(b)) + ' O\'s in a row')
+    print('X\'s and O\'s can be connected vertically, horizontally, or diagonally\n')
 
-    print('+---+')
+    print('+', end = '')
+    for x in b :
+        print('-', end = '')
+    print('+')
+
     i = 0
     for x in b :
         j = 0
@@ -17,7 +40,11 @@ def board(b) :
             j = j+1
         i = i+1
         print('|')
-    print('+---+')
+    
+    print('+', end = '')
+    for x in b :
+        print('-', end = '')
+    print('+')
 
 # code for user to input a move
 def user(b) :
@@ -27,8 +54,8 @@ def user(b) :
 
     while (len(move) != 3) or (move[1] != ',') \
         or (i.isnumeric() == False or j.isnumeric() == False) \
-        or (i != '0' and i != '1' and i != '2') \
-        or (j != '0' and j != '1' and j != '2') \
+        or (int(i) < 0 or int(i) > len(b)-1) \
+        or (int(j) < 0 or int(j) > len(b)-1) \
         or b[int(i)][int(j)] != ' ' :
 
         board(b)
@@ -45,37 +72,59 @@ def user(b) :
 
 # random move of bot
 def enemy(b) :
-    i = random.randint(0,2)
-    j = random.randint(0,2)
+    i = random.randint(0,len(b)-1)
+    j = random.randint(0,len(b)-1)
     while b[int(i)][int(j)] != ' ' :
-        i = random.randint(0,2)
-        j = random.randint(0,2)
+        i = random.randint(0,len(b)-1)
+        j = random.randint(0,len(b)-1)
     b[i][j] = 'O'
     board(b)
     return b
 
 def check_win(b) :
     # check for row win or loss
+    flag_X = True
+    flag_O = True
     i = 0
     for x in b :
-        if (b[i][0] == b[i][1]) and (b[i][0] == b[i][2]) and (b[i][0] != ' ') :
-            if(b[i][0] == 'X') :
-                print('\nYou Win')
-            else :
-                print('\nYou Lose')
+        j = 0
+        for y in b[i] :
+            if b[i][j] != 'X' :
+                flag_X = False
+            if b[i][j] != 'O' :
+                flag_O = False
+            j = j+1
+        if flag_X == True :
+            print('\nYou Win')
             return True
+        elif flag_O == True :
+            print('\nYou Lose')
+            return True
+        flag_X = True
+        flag_O = True
         i = i+1
 
     # check for column win or loss
-    j = 0
+    flag_X = True
+    flag_O = True
+    i = 0
     for x in b :
-        if (b[0][j] == b[1][j]) and (b[0][j] == b[2][j]) and (b[0][j] != ' ') :
-            if(b[0][j] == 'X') :
-                print('\nYou Win')
-            else :
-                print('\nYou Lose')
+        j = 0
+        for y in b[i] :
+            if b[j][i] != 'X' :
+                flag_X = False
+            if b[j][i] != 'O' :
+                flag_O = False
+            j = j+1
+        if flag_X == True :
+            print('\nYou Win')
             return True
-        j = j+1
+        elif flag_O == True :
+            print('\nYou Lose')
+            return True
+        flag_X = True
+        flag_O = True
+        i = i+1
 
     # check for main diagonal win or loss
     i = 0
@@ -96,7 +145,7 @@ def check_win(b) :
 
     # check for secondary diagonal win or loss
     i = 0
-    j = 2-i
+    j = len(b)-1-i
     flag_X = True
     flag_O = True
     for x in b :
@@ -131,7 +180,8 @@ def check_win(b) :
     return False
 
 
-b = [ [' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' '] ]
+size = input_size()
+b = [ [' ']*int(size) for i in range(int(size))]
 board(b)
 end = False
 while end == False :
@@ -143,3 +193,6 @@ while end == False :
     end = check_win(b)
     if(end == True) :
         break
+
+
+# note: make option for 2 player as well as 1 player against computer?
