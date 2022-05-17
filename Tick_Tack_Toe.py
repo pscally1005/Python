4268d2fc90f8306d8e74d3d745af4c49e3453c39
pscally1005 <mets1005@yahoo.com>
2022-05-17 09:44:38 -0400

    added 2P mode

--- Tick_Tack_Toe.py
+++ Tick_Tack_Toe.py
@@ -3,32 +3,54 @@ from os import system
 import random
 import getch
 
-# user inputs size of the board
-def input_size() :
+# 2 player or 1 player vs computer
+def mode() :
     system('clear')
     print('\nWelcome to Tic-Tac-Toe!')
+    print('\nWould you like to play 1P or 2P?', end = '')
+    print('\nPlease enter either "1" or "2": ', end = ' ')
+    m = getch.getche()
+    while (m.isnumeric() == False) or (int(m) < 1) or (int(m) > 2) :
+        system('clear')
+        print('\nWelcome to Tic-Tac-Toe!')
+        print('\nERROR: Mode type invalid', end = '')
+        print('\nWould you like to play 1P or 2P?', end = '')
+        print('\nPlease enter either "1" or "2": ', end = ' ')
+        m = getch.getche()
+    return m
+
+# user inputs size of the board
+def input_size(m) :
+    system('clear')
+    print('\nWelcome to Tic-Tac-Toe!\n')
+    print(str(m) + 'P mode selected')
     print('\nPlease Enter a Board Size from 3 to 9: ', end = ' ')
     size = getch.getche()
     while (size.isnumeric() == False) or (int(size) < 3) or (int(size) > 9) :
         system('clear')
-        print('\nWelcome to Tic-Tac-Toe!')
-        print('\nError: Board size invalid', end = '')
+        print('\nWelcome to Tic-Tac-Toe!\n')
+        print(str(m) + 'P mode selected')
+        print('\nERROR: Board size invalid', end = '')
         print('\nPlease Enter a Board Size from 3 to 9: ', end = ' ')
         size = getch.getche()
     return size
 
 # prints the board every time a move is made
-def board(b) :
+def board(b, m) :
     system('clear')
-    print('\nWelcome to Tic-Tac-Toe!')
-    print('\nYou will be player X.  The computer will be O')
-    print('You win by connecting ' + str(len(b)) + ' X\'s in a row. ', end = ' ')
-    print('You lose if the computer connects ' + str(len(b)) + ' O\'s in a row')
-    print('X\'s and O\'s can be connected vertically, horizontally, or diagonally\n')
+    print('\nWelcome to Tic-Tac-Toe!\n')
+    print(str(m) + 'P mode selected')
+    print(str(len(b)) + 'x' + str(len(b)) + ' board selected\n')
+
+    if int(m) == 1 :
+        print('You win by connecting ' + str(len(b)) + ' X\'s in a row')
+        print('You lose if the computer connects ' + str(len(b)) + ' O\'s in a row\n')
+    elif int(m) == 2 :
+        print('P1 wins by connecting ' + str(len(b)) + ' X\'s in a row.')
+        print('P2 wins by connecting ' + str(len(b)) + ' O\'s in a row\n')
 
     print('+', end = '')
-    for x in b :
-        print('-', end = '')
+    for x in b :    print('-', end = '')
     print('+')
 
     i = 0
@@ -42,46 +64,45 @@ def board(b) :
         print('|')
     
     print('+', end = '')
-    for x in b :
-        print('-', end = '')
+    for x in b :    print('-', end = '')
     print('+')
 
 # code for user to input a move
-def user(b) :
-    print('\nPlease enter a location (ex: 0,0):', end = ' ')
+def user(b, m, symbol) :
+    if int(m) == 1 :    print('\nPlease enter a location for X (ex: 0,0):', end = ' ')
+    elif int(m) == 2 :  print('\nPlease enter a location for ' + str(symbol) + ' (ex: 0,0) :', end = ' ')
     move = input()
-    [i, j] = [move[0], move[2]]
 
-    while (len(move) != 3) or (move[1] != ',') \
-        or (i.isnumeric() == False or j.isnumeric() == False) \
-        or (int(i) < 0 or int(i) > len(b)-1) \
-        or (int(j) < 0 or int(j) > len(b)-1) \
-        or b[int(i)][int(j)] != ' ' :
+    while (len(move) != 3) \
+        or (move[1] != ',') \
+        or (move[0].isnumeric() == False or move[2].isnumeric() == False) \
+        or (int(move[0]) < 0 or int(move[0]) > len(b)-1) \
+        or (int(move[2]) < 0 or int(move[2]) > len(b)-1) \
+        or b[int(move[0])][int(move[2])] != ' ' :
 
-        board(b)
-        print('\nERROR: Input is invalid')
-        print('Please enter a location (ex: 0,0):', end = ' ')
+        board(b, m)
+        print('\nERROR: Input is invalid', end = '')
+        if int(m) == 1 :    print('\nPlease enter a location for X (ex: 0,0):', end = ' ')
+        elif int(m) == 2 :  print('\nPlease enter a location for ' + str(symbol) + ' (ex: 0,0) :', end = ' ')
         move = input()
-        [i, j] = [move[0], move[2]]
 
-    board(b)
-    print('\nPlease enter a location (ex: 0,0): ' + str(move) + '\n')
-    b[int(i)][int(j)] = 'X'
-    board(b)
+    [i, j] = [move[0], move[2]]
+    b[int(i)][int(j)] = symbol
+    board(b, m)
     return b
 
 # random move of bot
-def enemy(b) :
+def enemy(b, m) :
     i = random.randint(0,len(b)-1)
     j = random.randint(0,len(b)-1)
     while b[int(i)][int(j)] != ' ' :
         i = random.randint(0,len(b)-1)
         j = random.randint(0,len(b)-1)
     b[i][j] = 'O'
-    board(b)
+    board(b, m)
     return b
 
-def check_win(b) :
+def check_win(b, m) :
     # check for row win or loss
     flag_X = True
     flag_O = True
@@ -89,16 +110,14 @@ def check_win(b) :
     for x in b :
         j = 0
         for y in b[i] :
-            if b[i][j] != 'X' :
-                flag_X = False
-            if b[i][j] != 'O' :
-                flag_O = False
+            if b[i][j] != 'X' : flag_X = False
+            if b[i][j] != 'O' : flag_O = False
             j = j+1
         if flag_X == True :
-            print('\nYou Win')
+            print('\nYou Win') if int(m) == 1 else print('\nP1 Wins')
             return True
         elif flag_O == True :
-            print('\nYou Lose')
+            print('\nYou Lose') if int(m) == 1 else print('\nP2 Wins')
             return True
         flag_X = True
         flag_O = True
@@ -111,16 +130,14 @@ def check_win(b) :
     for x in b :
         j = 0
         for y in b[i] :
-            if b[j][i] != 'X' :
-                flag_X = False
-            if b[j][i] != 'O' :
-                flag_O = False
+            if b[j][i] != 'X' : flag_X = False
+            if b[j][i] != 'O' : flag_O = False
             j = j+1
         if flag_X == True :
-            print('\nYou Win')
+            print('\nYou Win') if int(m) == 1 else print('\nP1 Wins')
             return True
         elif flag_O == True :
-            print('\nYou Lose')
+            print('\nYou Lose') if int(m) == 1 else print('\nP2 Wins')
             return True
         flag_X = True
         flag_O = True
@@ -131,16 +148,14 @@ def check_win(b) :
     flag_X = True
     flag_O = True
     for x in b :
-        if b[i][i] != 'X' :
-            flag_X = False
-        if b[i][i] != 'O' :
-            flag_O = False
+        if b[i][i] != 'X' : flag_X = False
+        if b[i][i] != 'O' : flag_O = False
         i = i+1
     if flag_X == True :
-        print('\nYou Win')
+        print('\nYou Win') if int(m) == 1 else print('\nP1 Wins')
         return True
     elif flag_O == True :
-        print('\nYou Lose')
+        print('\nYou Lose') if int(m) == 1 else print('\nP2 Wins')
         return True
 
     # check for secondary diagonal win or loss
@@ -149,17 +164,15 @@ def check_win(b) :
     flag_X = True
     flag_O = True
     for x in b :
-        if b[i][j] != 'X' :
-            flag_X = False
-        if b[i][j] != 'O' :
-            flag_O = False
+        if b[i][j] != 'X' : flag_X = False
+        if b[i][j] != 'O' : flag_O = False
         i = i+1
         j = j-1
     if flag_X == True :
-        print('\nYou Win')
+        print('\nYou Win') if int(m) == 1 else print('\nP1 Wins')
         return True
     elif flag_O == True :
-        print('\nYou Lose')
+        print('\nYou Lose') if int(m) == 1 else print('\nP2 Wins')
         return True
     
     # check for tie game
@@ -179,20 +192,22 @@ def check_win(b) :
 
     return False
 
+def main() :
+    m = mode()
+    size = input_size(m)
+    b = [ [' ']*int(size) for i in range(int(size))]
+    board(b, m)
+    end = False
+    while end == False :
+        b = user(b, m, 'X')
+        end = check_win(b, m)
+        if(end == True) :   break
 
-size = input_size()
-b = [ [' ']*int(size) for i in range(int(size))]
-board(b)
-end = False
-while end == False :
-    b = user(b)
-    end = check_win(b)
-    if(end == True) :
-        break
-    b = enemy(b)
-    end = check_win(b)
-    if(end == True) :
-        break
+        if int(m) == 1 :    b = enemy(b, m)
+        elif int(m) == 2 :  b = user(b, m, 'O')
 
+        end = check_win(b, m)
+        if(end == True) :   break
 
-# note: make option for 2 player as well as 1 player against computer?
+if __name__ == "__main__" :
+    main()
---
+++ test.py
@@ -0,0 +1,4 @@
+str1 = 'hello world'
+i = 0
+for x in str1 :
+    print(x)
